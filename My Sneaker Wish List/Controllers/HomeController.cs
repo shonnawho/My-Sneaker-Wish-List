ecaec2a8fd95f42ae9d4770d1fd9d95d1070619c
Shonna <peshonna@gmail.com>
2018-07-31 21:25:57 -0500

    Add project files.

---
+++ My Sneaker Wish List/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using My_Sneaker_Wish_List.Models;
+
+namespace My_Sneaker_Wish_List.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ My Sneaker Wish List/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace My_Sneaker_Wish_List.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
