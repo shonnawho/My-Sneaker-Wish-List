28c74e2f01f0c8dafecb7da032b7529c6f32699c
Peshonna Parnell <peshonna@gmail.com>
2019-11-08 00:44:45 -0600

    im lazy

--- My Sneaker Wish List/Controllers/MenuController.cs
+++ My Sneaker Wish List/Controllers/MenuController.cs
@@ -1,1 +1,118 @@
+using CheeseMVC.Data;
+using CheeseMVC.Models;
+using CheeseMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CheeseMVC.Controllers
+{
+    public class MenuController : Controller
+
+    {
+        private readonly CheeseDbContext context;
+
+        //private CheeseDbContext context;
+
+        public MenuController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<Menu> menus = context.Menus.ToList();
+
+            return View(menus);
+        }
+
+        public IActionResult Add()
+        {
+
+            AddMenuViewModel addMenuViewModel = new AddMenuViewModel();
+            return View(addMenuViewModel);
+
+        }
+        [HttpPost]
+        public IActionResult Add(AddMenuViewModel addMenuViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Menu newMenu = new Menu
+                {
+                    Name = addMenuViewModel.Name
+                };
+
+                context.Menus.Add(newMenu);
+                context.SaveChanges();
+
+                return Redirect("/Menu/ViewMenu/" + newMenu.ID);
+            }
+
+            return View(addMenuViewModel);
+        }
+        public IActionResult ViewMenu(int id)
+        {
+
+         List<CheeseMenu> items = context
+            .CheseseMenus
+            .Include(item => item.Cheese)
+            .Where(cm => cm.MenuID == id)
+            .ToList();
+
+         Menu menu = context.Menus.Single(m => m.ID == id);
+
+            ViewMenuViewModel viewModel = new ViewMenuViewModel
+            {
+                Menu = menu,
+                Items = items
+            };
+
+            return View(viewModel);
+        }
+
+        public IActionResult AddItem(int id)
+        {
+            Menu menu = context.Menus.Single(m => m.ID == id);
+            List<Cheese> cheeses = context.Cheeses.ToList();
+            return View(new AddMenuItemViewModel(menu, cheeses));
+        }
+
+        [HttpPost]
+        public IActionResult AddItem(AddMenuItemViewModel addMenuItemViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var cheeseID = addMenuItemViewModel.CheeseID;
+                var menuID = addMenuItemViewModel.MenuID;
+
+                List<CheeseMenu> existingItems = context.CheseseMenus
+                    .Where(cm => cm.CheeseID == cheeseID)
+                    .Where(cm => cm.MenuID == menuID).ToList();
+                if (existingItems.Count == 0)
+                {
+
+                    CheeseMenu menuItem = new CheeseMenu
+                    {
+                        Cheese = context.Cheeses.Single(c => c.ID == cheeseID),
+                        Menu = context.Menus.Single(m => m.ID == menuID)
+                    };
+
+                    context.CheseseMenus.Add(menuItem);
+                    context.SaveChanges();
+
+                }
+
+                return Redirect(string.Format("/Menu/ViewMenu/{0}", addMenuItemViewModel.MenuID));
+
+            }
+
+            return View(addMenuItemViewModel);
+                
+        } 
+
+    }
+
+}
 ﻿
